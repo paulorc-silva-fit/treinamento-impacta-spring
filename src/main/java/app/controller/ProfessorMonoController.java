a07a8da99cfee74def01384efb7b290fbc02267d
paulorc-silva-fit <paulo.costa@aluno.faculdadeimpacta.com.br>
2023-01-20 21:19:12 -0300

    Finalizando crud de professores

--- src/main/java/app/controller/ProfessorMonoController.java
+++ src/main/java/app/controller/ProfessorMonoController.java
@@ -2,6 +2,7 @@ package app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,8 +25,8 @@ public class ProfessorMonoController {
 	public ModelAndView listarProfessores() {
 		ModelAndView mvProf = new ModelAndView("listarProfessor");
 		Iterable<Professor> professores = profRepo.findAll();
-		
 		mvProf.addObject("professores", professores);
+
 		return mvProf;
 	}
 	
@@ -42,4 +43,31 @@ public class ProfessorMonoController {
 		profRepo.save(prof);
 		return "redirect:/listar-professores";
 	}
+	
+	@RequestMapping(value="/{matricula}", method=RequestMethod.GET)
+	public ModelAndView editarProfessor(@PathVariable("matricula") long matricula) {
+		Professor prof = profRepo.findById(matricula);
+		ModelAndView mvProf = new ModelAndView("editarProfessor");
+		mvProf.addObject("professor", prof);
+		
+		return mvProf;
+	}
+	
+	@RequestMapping(value="/{matricula}", method=RequestMethod.POST)
+	public String editarProfessorPost(Professor prof) {
+		ProfessorService profService = new ProfessorService();
+		
+		prof.setSalario(profService.calcularSalario(prof.getQtdHorasTrabalhadas(), prof.getValorHoraTrabalhada()));
+		profRepo.save(prof);
+		return "redirect:/listar-professores";
+	}
+	
+	@RequestMapping("/deletar-professor")
+	public String deletarProfessor(long matricula) {
+		Professor prof = profRepo.findById(matricula);
+		profRepo.delete(prof);
+		
+		return "redirect:/listar-professores";
+	}
+	
 }
